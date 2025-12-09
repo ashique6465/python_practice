f3c9facf7d5414b2df846a62cd4f10c5fac9f7c8
Md Ashique Ali <143936710+ashique6465@users.noreply.github.com>
2025-12-09 10:44:07 +0000

    Solve Question

--- practice.py
+++ practice.py
@@ -1,8 +1,26 @@
-import re
-def text_match(text):
-    pattern = r'\w+\S*$'
-    ans = re.search(pattern,text)
-    return True if ans else False
+# import re
+# text = '192.168.1.1 - - [01/Dec/2025:10:30:45 +0000] "GET /api/users HTTP/1.1" 200'
+# pattern = re.compile(r'\b(?P<ip>(?:\d{1,3}\.){3}\d{1,3})(?::(?P<port>\d{1,3}))?\b.*?(?P<timestamp>\d{1,2}/[A-Za-z]{3}/\d{4}:\d{2}:\d{2}:\d{2}\s[+\-]\d{4})')
+# m = pattern.search(text)
+# if m:
+#     print(m.group('ip'))
+#     print(m.group('port'))
+#     print(m.group('timestamp'))
+# else:
+#     print('No match')
 
-print(text_match("The quick brown fox jumps over the lazy dog."))
-print(text_match("Python Exercises."))
+import re
+text = """(555) 123-4567
+555-123-4567
+555.123.4567
+5551234567"""
+pattern = re.compile(r"""(?P<area>\d{3}) \D*(?P<prefix>\d{3})\D*(?P<suffix>\d{4})""", re.VERBOSE)
+
+normalize = []
+for match in pattern.finditer(text):
+    area = match.group("area")
+    prefix = match.group("prefix")
+    suffix = match.group("suffix")
+
+normalize.append(f'{area}-{prefix}-{suffix}')
+print(''.join(normalize))
