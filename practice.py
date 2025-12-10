9f35edc039b71d2e5bc30416ba84582ba52d960e
Md Ashique Ali <143936710+ashique6465@users.noreply.github.com>
2025-12-10 11:40:03 +0000

    Solve

--- practice.py
+++ practice.py
@@ -1,15 +1,10 @@
-def Solve(nums):
-    Freq = {}
-    for n in nums:
-        if n not in Freq:
-            Freq[n] = 1 
-        else:
-            Freq[n] += 1 
-    res = [] 
-    for e , v in Freq.items():
-        if v > len(nums) // 3 :
-            res.append(v)
-    return res, len(nums) // 3
+import re 
+def IP(test_ips):
+    pattern = r'^(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}$'
+    regex = re.compile(pattern)
+    for ip in test_ips:
+        print(f'{ip} => {bool(regex.match(ip))}')
 
-nums = [ 3, 2, 3]
-print(Solve(nums))
+test_ips = ["192.168.0.1", "10.0.0.255", "255.255.255.255", "256.100.50.25", "192.168.0", "abc.def.ghi.jkl"]
+
+print(IP(test_ips))
