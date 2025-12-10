38ecdff059dea690083b12505cce6734bc18d1e3
Md Ashique Ali <143936710+ashique6465@users.noreply.github.com>
2025-12-10 10:09:56 +0000

    Solved Problems

--- practice.py
+++ practice.py
@@ -1,26 +1,15 @@
-# import re
-# text = '192.168.1.1 - - [01/Dec/2025:10:30:45 +0000] "GET /api/users HTTP/1.1" 200'
-# pattern = re.compile(r'\b(?P<ip>(?:\d{1,3}\.){3}\d{1,3})(?::(?P<port>\d{1,3}))?\b.*?(?P<timestamp>\d{1,2}/[A-Za-z]{3}/\d{4}:\d{2}:\d{2}:\d{2}\s[+\-]\d{4})')
-# m = pattern.search(text)
-# if m:
-#     print(m.group('ip'))
-#     print(m.group('port'))
-#     print(m.group('timestamp'))
-# else:
-#     print('No match')
+def Solve(nums):
+    Freq = {}
+    for n in nums:
+        if n not in Freq:
+            Freq[n] = 1 
+        else:
+            Freq[n] += 1 
+    res = [] 
+    for e , v in Freq.items():
+        if v > len(nums) // 3 :
+            res.append(v)
+    return res, len(nums) // 3
 
-import re
-text = """(555) 123-4567
-555-123-4567
-555.123.4567
-5551234567"""
-pattern = re.compile(r"""(?P<area>\d{3}) \D*(?P<prefix>\d{3})\D*(?P<suffix>\d{4})""", re.VERBOSE)
-
-normalize = []
-for match in pattern.finditer(text):
-    area = match.group("area")
-    prefix = match.group("prefix")
-    suffix = match.group("suffix")
-
-normalize.append(f'{area}-{prefix}-{suffix}')
-print(''.join(normalize))
+nums = [ 3, 2, 3]
+print(Solve(nums))
